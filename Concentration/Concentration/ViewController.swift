df56dd56e25a0b0d1f12f4a1ff89c831678eece8
pawel misiak <pawelmisiak@live.com>
2018-09-23 13:24:08 -0400

    changed theme to have inverse background from the buttons

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,27 +8,30 @@
 
 import UIKit
 
+extension UIColor {
+    func inverse () -> UIColor {
+        var r:CGFloat = 0.0; var g:CGFloat = 0.0; var b:CGFloat = 0.0; var a:CGFloat = 0.0;
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
+            return UIColor(red: 1.0-r, green: 1.0 - g, blue: 1.0 - b, alpha: a)
+        }
+        return .black // Return a default colour
+    }
+}
 
 class ViewController: UIViewController { //ViewController extends UIViewController which means it inherits here
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = correspondingThemeColor[currentThemeNumber].inverse()
+    }
+
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
     
     private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
-    
-    var numberOfPairsOfCards: Int {
-            return (cardButtons.count + 1) / 2
-    }
-    //the lazy saids not to use it until it is called (create the var when you use it)
-    //lazy variable cannot have didSet
-    let themes = [
-        ["👻","🎃","☠️","👹","😈","🧟‍♂️","🧛🏻‍♂️","☄️","🍬"],
-        ["🤾‍♀️","🏊‍♂️","🥊","🏈","🚴‍♂️","🏓","🏌🏻‍♂️","⚽️","🎳"],
-        ["🍝","🥓","🍜","🥞","🍕","🍟","🍔","🌮","🌭"],
-        ["🇵🇱","🇺🇸","🇵🇹","🇦🇷","🇨🇦","🇮🇹","🇩🇪","🇯🇵","🏴󠁧󠁢󠁥󠁮󠁧󠁿"],
-        ["🛸","🛥","🚂","🚅","🚲","🚜","🚗","✈️","🚀"],
-        ["😇","😤","😑","🤢","😱","😂","😎","😡","😀"],
-    ]
-    let correspondingThemeColor = [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)]
-    var currentThemeNumber = 0
-    
+
     @IBAction func newGame() {
         game.numberOfFlips = 0  // This var is reset here instead when init because it is refreshed to 0 on press
         game.points = 0 // The same reason as above
@@ -38,27 +41,27 @@ class ViewController: UIViewController { //ViewController extends UIViewControll
             game.cards[index].isFaceUp = false
             game.cards[index].isMatched = false
         }
+        viewDidLoad()
         updateViewFromModel()
         game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     }
+
+    //the lazy saids not to use it until it is called (create the var when you use it)
+    //lazy variable cannot have didSet
+    let themes = [
+        ["👻","🎃","☠️","👹","😈","🧟‍♂️","🧛🏻‍♂️","☄️","🍬"],
+        ["🤾‍♀️","🏊‍♂️","🥊","🏈","🚴‍♂️","🏓","🏌🏻‍♂️","⚽️","🎳"],
+        ["🍝","🥓","🍜","🥞","🍕","🍟","🍔","🌮","🌭"],
+        ["🇵🇱","🇺🇸","🇵🇹","🇦🇷","🇨🇦","🇮🇹","🇩🇪","🇯🇵","🏴󠁧󠁢󠁥󠁮󠁧󠁿"],
+        ["🛸","🛥","🚂","🚅","🚲","🚜","🚗","✈️","🚀"],
+        ["😇","😤","😑","🤢","😱","😂","😎","😡","😀"],
+        ]
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    let correspondingThemeColor = [#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1),#colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)]
+    var currentThemeNumber = 0
     
-    @IBOutlet weak var ScoreCountLabel: UILabel!
-    
-    @IBOutlet var cardButtons: [UIButton]!
-    
-    
-//    var flipCount = 0 {
-//        didSet { //whenever someone changes the flip count it will change the lable on the screen
-//            flipCountLabel.text = "Flips: \(flipCount)"
-//        }
-//    }
-    
-    //var emoji = Dictionary<Int, String>() // Dictionary is a struct and it hast to be initialized
-    var emoji = [Int: String]() // the same as above
-    
-    var emojiChoices = ["👻","🎃","☠️","👹","😈","🧟‍♂️","🧛🏻‍♂️","☄️","🍬"]
+    var emoji = [Int: String]() // Creates a dictionary with strings and ints
+    var emojiChoices = ["👻","🎃","☠️","👹","😈","🧟‍♂️","🧛🏻‍♂️","☄️","🍬"] // for some reason I cannot make this equal to themes[0]
     
     private func getThemeNumber() -> Int {
         for idx in themes.indices {
@@ -68,6 +71,12 @@ class ViewController: UIViewController { //ViewController extends UIViewControll
         }
         return 0
     }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet weak var ScoreCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
     
     func emoji(for card: Card) -> String { // calling for internally card
         if emoji[card.identifier] == nil {
@@ -103,9 +112,8 @@ class ViewController: UIViewController { //ViewController extends UIViewControll
                 currentButton.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
             } else {
                 currentButton.setTitle("", for: UIControlState.normal)
-                currentButton.backgroundColor = currentCard.isMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) : correspondingThemeColor[currentThemeNumber]
+                currentButton.backgroundColor = currentCard.isMatched ? correspondingThemeColor[currentThemeNumber].inverse() : correspondingThemeColor[currentThemeNumber]
             }
         }
     }
 }
-
